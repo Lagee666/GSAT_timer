ba0e5722ac7bedda76c12497bb0282bd5ec02773
Lagee666 <z29716581@gmail.com>
2025-07-16 16:51:32 +0800

    Update gh pagh config and use HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Countdown from './pages/Countdown';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/countdown" element={<Countdown />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/countdown" element={<Countdown />} />
+    </Routes>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/GSAT_timer' : '/', // 生產環境用 /GSAT_timer，開發環境用 /
+    plugins: [react()],
+  };
 });
